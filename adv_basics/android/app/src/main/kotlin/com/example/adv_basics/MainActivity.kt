be3a679df17b2df3e8661c6cad5e22f03d1ac928
Aadeesh11 <asoswal@gmail.com>
2024-07-22 17:21:28 +0530

    a

--- adv_basics/android/app/src/main/kotlin/com/example/adv_basics/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.adv_basics
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- adv_basics/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:adv_basics/quiz.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp( const Quiz()
-  );
-}
